de752e4fa6e66f45a68537bca0fa97d205e4c8c9
WSL windows10/ubuntu <g...@hotmail.com>
2021-09-16 22:22:14 -0300

    Organizing

---
+++ data_section/module.py
@@ -0,0 +1,193 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy.optimize import fsolve
+
+    
+def X(Ce, Cs):
+    """
+    Return the reaction conversion
+    
+    Parameters:
+    -----------
+    :param Ce (int/numpy.array): Initial concentration
+    :param Cs (int/numpy.array): Final concentration
+    
+    :return (float): Conversion
+    """
+    return 1 - Cs/Ce
+
+def generate_values(Vmin, Vmax, n):
+    """
+    Generates n random values from a range defined by Vmin and Vmax
+    
+    Parameters:
+    -----------
+    :param Vmin (float): minimum side of the range
+    :param Vmax (float): maximum side of the range
+    :param n (int): number of data
+    
+    :return (numpy.array): Random values of that range
+    """
+    V_range = np.linspace(Vmin, Vmax, int(.005*n)+2)
+    f_V_range = lambda x: V_range[x]
+    
+    V = (np.random.rand(n) - 0.5)*(Vmax - Vmin)/10
+    V += f_V_range( np.random.randint(0, V_range.shape[0], n) )
+    return abs(V)
+
+class steadyState:
+    """
+    Define a EDO system that can be used to evaluate the steady state as well the reactor startup
+    """
+    def __init__(self, data, isothermal=True):
+        """
+        Instanciate a class with the initial data as well the properties data.
+        
+        Parameters:
+        -----------
+        :param data (pandas.DataFrame): Initial points of concentrations, temperature, constante rate, spacial time and thermal properties [Cae, Cbe, Cce, Cde, T, k01, Ea1, k02, Ea2, t_spacial, dHr, rho, Cp]
+        :param isothermal (boolean): If the ractor will be operated in a isothermal (True) or adiabatic state (False).
+        """
+        self.Cae = data['Cae']
+        self.Cbe = data['Cbe']
+        self.Cce = data['Cce']
+        self.Cde = data['Cde']
+        
+        self.k01 = data['k01']
+        self.Ea1 = data['Ea1']
+        
+        self.k02 = data['k02']
+        self.Ea2 = data['Ea2']
+        
+        self.T = data['T']
+        self.t_spacial = data['t_spacial']
+        self.dHr = data['dHr']
+        self.rho = data['rho']
+        self.Cp = data['Cp']
+        
+        self.isothermal = isothermal
+    
+    def constant_rate(self, T, k0, Ea, R=8.314):
+        """
+        Evaluate the constant rate
+        
+        Parameters:
+        -----------
+        :param k0 (float): Pre-exponential factor [1/min]
+        :param Ea (float): Activation energy [J/ mol]
+        :param R (float): Ideal gas constant [J/ K.mol]
+        :param T (float): Temperatura [K]
+
+        :return (float):
+        """
+        return k0*np.exp(-Ea/(R*T))
+    
+    def ra(self, Ca, Cb, Cc, Cd, T):
+        """
+        Evaluate the reaction rate
+        
+        Parameters:
+        -----------
+        :param Ca (float): Concentration of component A
+        :param Cb (float): Concentration of component B
+        :param Cc (float): Concentration of component C
+        :param Cd (float): Concentration of component D
+        :param T (float): Temperature
+        """
+        k1 = self.constant_rate(T, self.k01, self.Ea1)
+        k2 = self.constant_rate(T, self.k02, self.Ea2)
+        return -k1*Ca*Cb + k2*Cc*Cd
+        
+    def dcadt(self, C, t):
+        """
+        Used to evaluate the EDO system at time t, using data from time t-1
+        
+        Parameters:
+        -----------
+        :param C (numpy.array): Array with values of concentration and temperature at t-1
+        :param t (numpy.array): Array with the time array which the EDO system are being evaluated
+        """
+        Ca, Cb, Cc, Cd, T = C
+        dcdt = [0, 0, 0, 0, 0]
+        ra = self.ra(Ca, Cb, Cc, Cd, T)
+        
+        dcdt[0] = (self.Cae-Ca)/self.t_spacial + ra
+        dcdt[1] = (self.Cbe-Cb)/self.t_spacial + ra
+        dcdt[2] = (self.Cce-Cc)/self.t_spacial - ra
+        dcdt[3] = (self.Cde-Cd)/self.t_spacial - ra
+        if self.isothermal:
+            dcdt[4] = 0
+        else:
+            dcdt[4] = (self.T - T)/self.t_spacial - self.dHr*ra/(self.rho* self.Cp)
+        return dcdt
+
+
+def ss_solve(data, isothermal=True):
+    """
+    Solve the ODE system for the steady state, for all initial points in data
+    
+    Parameters:
+    -----------
+    :param data (pandas.DataFrame): Initial points of concentrations, temperature, constante rate, spacial time and thermal properties [Cae, Cbe, Cce, Cde, T, k01, Ea1, k02, Ea2, t_spacial, dHr, rho, Cp]
+    :param isothermal (boolean): If the ractor will be operated in a isothermal (True) or adiabatic state (False).
+    """
+    out = []
+    for row in range( data.shape[0] ):
+        s = steadyState(data.loc[row], isothermal)
+        out.append(fsolve(s.dcadt,
+                         [s.Cae, s.Cbe, s.Cce, s.Cde, s.T]))
+    return pd.DataFrame(out, columns=['Ca', 'Cb', 'Cc', 'Cd', 'T'])
+
+def ode_solve(data, n, t=10, row=0, isothermal=True):
+    """
+    Solve a ODE system based on initial values in data
+    
+    Parameters:
+    -----------
+    :param data (pandas.DataFrame): 
+    :param n (int): Number of points
+    :param t (float): Maximum time, in seconds, which the system will be evaluated
+    :param row (int): Which row of data will be evaluated
+    :param isothermal (boolean): If the ractor will be operated in a isothermal (True) or adiabatic state (False).
+    
+    :return (pandas.DataFrame): Solution through time of the system.
+    """
+    t = np.linspace(0, t, n)
+    s = steadyState(data.loc[row], isothermal)
+    out = pd.DataFrame(odeint(s.dcadt,
+                              [s.Cae, s.Cbe, s.Cce, s.Cde, s.T],
+                              t),
+                       columns=['Ca', 'Cb', 'Cc', 'Cd', 'T'], index=t)
+    out = out[out>=0].dropna()
+    return out
+
+def ode_plot(ode_solved):
+    """
+    Ploting method of the startup of a reactor
+    
+    Parameters:
+    -----------
+    :param ode_solved (pandas.DataFrame): Solution of a the ODE system
+    """
+    plt.figure(figsize=(11,6))
+    axis_x = ode_solved.index/60
+    ax1 = plt.plot(axis_x, ode_solved[['Ca', 'Cb', 'Cc', 'Cd']]*100/ode_solved['Ca'][0], label=['Ca', 'Cb', 'Cc', 'Cd'], linewidth=2)
+    plt.xlabel('tempo [min]')
+    plt.ylabel('%')
+    plt.grid()
+    
+    # Temperature axis
+    ax2 = plt.twinx()
+    ax2 = ax2.plot(axis_x, ode_solved[['T']], label='T', color='black', linewidth=3)
+    
+    # complements
+    plt.xlabel('tempo [s]')
+    plt.ylabel('T [K]')
+
+    ax = ax1 + ax2
+    legends = [l.get_label() for l in ax]
+    plt.legend(ax, legends, loc='best', ncol=5, )
+
+    plt.show()
--- model_section/utils.py
+++ model_section/utils.py
@@ -1,9 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error as mse_f, r2_score
+from keras.models import load_model
 
 
 def plot_identity_graphic(data_true, data_pred):
+    """
+    Plot an identity graphic based on the true and predicted values
+    
+    Parameters:
+    -----------
+    :param data_true (numpy.array): true values of the comparison plot
+    :param data_pred (numpy.array): predicted values of the comparison plot
+    
+    :return: None
+    """
     plt.figure(figsize=(13, 6))
     m, b = np.polyfit(data_true, data_pred, 1)
     limit_high = np.max([data_pred.max(), data_true.max()])
@@ -20,24 +31,78 @@ def plot_identity_graphic(data_true, data_pred):
     plt.title(f'$R^2$={r2:.2f}, RMSE={np.sqrt(mse):.2f}, RMSRE: {RMSRE(data_true, data_pred, order=1):.2f} %,  RMSRE: {RMSRE(data_true, data_pred):.2f} %', fontsize=10)
     plt.grid(color='gray', linestyle='--', linewidth=0.25)
     
+    return
+    
 def plot_identity_keras(model, X_test, Y_test):
     """
+    Plot an identity graphic based on a model output from keras package
+    
+    Parameters:
+    -----------
+    :param model (keras.engine.sequential): trained keras model
+    :param X_test (numpy.array): input data from the test set
+    :param Y_test (numpy.array): output data from the test set
+    
+    :return: None
     """
     Y_hat_test = model.predict(X_test)
     Y_hat_test = Y_hat_test.reshape(Y_hat_test.shape[0],)
     plot_identity_graphic(Y_test, Y_hat_test)
+    
+    return
+
+def plot_loss_history(hist):
+    """
+    Plot losses from the train and validation sets through epocs
+    
+    Parameters:
+    -----------
+    :param hist (keras.callbacks.History): output from keras' fit method
+    
+    :return: None
+    """
+    plt.figure(figsize=(13,7))
+    plt.plot(hist.history['loss'], label='train set', linewidth=2)
+    plt.plot(hist.history['val_loss'], label='validation set', linewidth=3)
+    plt.ylabel('mean sqared error')
+    plt.xlabel('Number of epochs')
+    plt.legend()
+    plt.grid()
+    
+    return
 
 
-def plot_boxplot_input(data, data_labels, name=''):
+def plot_boxplot_input(data, data_labels):
+    """
+    Plot a boxplot graphic of the input data
+    
+    Parameters:
+    -----------
+    :param data (pandas.DataFrame): input data
+    :param data_labels (list-like): input data names
+    
+    :return: None
+    """
     plt.figure(figsize=(6, 4))
     plt.boxplot(data)
     plt.xticks(np.arange(len(data_labels))+1, data_labels, rotation='45')
     plt.grid(':', linewidth=0.25)
-    plt.title('Boxplot of the ' + name + ' variables')
+    
+    return
 
 def RMSRE(true_values, predict_values, order=2):
     """
-    Evaluate metric
+    Root mean squared relative error metric, evaluates on average how much the predicted is off to the true value.
+    
+    Parameters:
+    -----------
+    :param data_true (numpy.array): true values of the comparison plot
+    :param data_pred (numpy.array): predicted values of the comparison plot
+    :param order (int): 
+    
+    :return (float): RMSRE
+    
+    Note: mean( ((predict_values-true_values)/true_values)**order )
     """
     relativeSE = ((predict_values-true_values)*100/true_values)**order
     MrelativeSE = np.mean( relativeSE )
@@ -52,6 +117,16 @@ def RMSRE(true_values, predict_values, order=2):
 
 
 def plot_first_layer_keras(model, label=None):
+    """
+    Plot the weights from the first layer of the model
+    
+    Parameters:
+    -----------
+    :param model (keras.engine.sequential): trained keras model
+    :param label (list): name of each input
+    
+    :return: None
+    """
     plt.figure(figsize=(10,6))
     first_layer = model.get_weights()[0]
     plt.imshow(abs(first_layer).T, cmap='Greys', aspect='auto', interpolation='bicubic', origin='lower')
@@ -66,10 +141,21 @@ def plot_first_layer_keras(model, label=None):
     plt.ylabel('Neuron index', fontsize=10)
     plt.grid()
     plt.show()
-    
+    return
     
     
 def plot_rnn_keras(model):
+    """
+    Plot the configuration of the ANN
+    
+    Parameters:
+    -----------
+    :param model (keras.engine.sequential): trained keras model
+    
+    :return: None
+    
+    Note: best to visualize for networks with not so many layers nor neurons
+    """
     def nodes(model):
         lim_max = int(len(model)/4)
         lim_min = -int(len(model)/4)
@@ -111,25 +197,36 @@ def plot_rnn_keras(model):
     
 def evaluate(model, X_train, X_test, Y_train, Y_test):
     """
+    Evaluate the RMSRE metric for the train and for the test set
     
+    Parameters:
+    -----------
+    :param model (keras.engine.sequential): trained keras model
+    :param X_train (numpy.array): input data from the train set
+    :param X_test (numpy.array): input data from the test set
+    :param Y_train (numpy.array): output data from the train set
+    :param Y_test (numpy.array): output data from the test set
+    
+    :return: None
     """
     Y_hat_train = model.predict(X_train)
     Y_hat_train = Y_hat_train.reshape(Y_hat_train.shape[0],)
     Y_hat_test = model.predict(X_test)
     Y_hat_test = Y_hat_test.reshape(Y_hat_test.shape[0],)
     
-    print(f'Train: {mse_f(Y_train, Y_hat_train):.4f}')
-    print(f'Test: {mse_f(Y_test, Y_hat_test):.4f}')
+    print(f'Train: {RMSRE(Y_train, Y_hat_train):.4f}')
+    print(f'Test: {RMSRE(Y_test, Y_hat_test):.4f}')
     return
 
-def load_model(path='500_dados.hdf5'):
+def load_model(path='models/500_dados.hdf5'):
     """
+    Load a pre trained model from keras
     
+    Parameters:
+    -----------
+    :param path (str): path to the model
+    
+    :return: (keras.engine.sequential)
     """
-    model = Sequential()
-    model.add(Dense(25, input_dim=X_train.shape[1], activation='exponential'))
-    model.add(Dense(11, activation='sigmoid', ))
-    model.add(Dense(3, activation='exponential'))
-    model.add(Dense(1))
-    model.load_weights('500_dados.hdf5')
+    model = load_model(path)
     return model
